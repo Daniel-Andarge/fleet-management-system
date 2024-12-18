8c6c829475fb5c8646f2ed4256e7a582691a25f1
Daniel-Andarge <andargedaniel90@gmail.com>
2024-12-18 23:51:40 +0300

    Add Server

--- server/server.js
+++ server/server.js
@@ -6,31 +6,25 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Import routes
 const vehicleRoutes = require('./routes/vehicleRoutes');
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => {
     console.error('MongoDB connection error:', err);
-    process.exit(1); // Exit process if MongoDB connection fails
+    process.exit(1);
   });
 
-// Routes
-app.use('/routes', vehicleRoutes); // Base path for vehicle routes
+app.use('/routes', vehicleRoutes);
 
-// Root Route for Testing
 app.get('/', (req, res) => {
   res.send('Welcome to the Vehicle Management API!');
 });
 
-// Start the Server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
